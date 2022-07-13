d580c427e57e21c1386f650bd3716b5140edeb2d
rymiyamoto <ry.miyamoto129@gmail.com>
2022-07-13 10:17:24 +0900

    feat: add

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/rymiyamoto/worker-pool/pool"
+	"github.com/rymiyamoto/worker-pool/work"
+)
+
+const (
+	workerCount = 5
+	jobCount    = 100
+)
 
 func main() {
-	log.Print("hello")
+	log.Println("starting application...")
+
+	collector := pool.StartDispatcher(workerCount) // start up worker pool
+
+	for i, job := range work.CreateJobs(jobCount) {
+		collector.Work <- pool.Work{Job: job, ID: i}
+	}
 }
