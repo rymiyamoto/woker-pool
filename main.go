f7452c8ae04269891326c2227adc26c8bd2f1121
rymiyamoto <ry.miyamoto129@gmail.com>
2022-07-16 22:05:35 +0900

    feat: set echo endpoint

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"net/http"
 
+	"github.com/labstack/echo/v4"
 	"github.com/rymiyamoto/worker-pool/pool"
 	"github.com/rymiyamoto/worker-pool/work"
 )
@@ -13,11 +15,19 @@ const (
 )
 
 func main() {
-	log.Println("starting application...")
-
 	collector := pool.StartDispatcher(workerCount) // start up worker pool
 
-	for i, job := range work.CreateJobs(jobCount) {
-		collector.Work <- pool.Work{Job: job, ID: i}
-	}
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		go func() {
+			log.Println("starting application...")
+
+			for i, job := range work.CreateJobs(jobCount) {
+				collector.Work <- pool.Work{Job: job, ID: i}
+			}
+		}()
+
+		return c.String(http.StatusOK, "queued!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
